78e27a37d0930d26de69acb9a7b98b477b88b14b
DaviQueirozLima <daviqueirozlima6@gmail.com>
2026-03-23 12:34:25 -0300

    feat: :sparkles: implementação do controller

---
+++ TodoApi/Controllers/TasksController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using TodoApi.Service;
+using TodoApi.Services;
+
+namespace TodoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private readonly ITaskService _service;
+
+        public TasksController(ITaskService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var tasks = await _service.GetAll();
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var task = await _service.GetById(id);
+            if (task == null) return NotFound();
+
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TaskItem task)
+        {
+            var created = await _service.Create(task);
+            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, TaskItem task)
+        {
+            var updated = await _service.Update(id, task);
+            if (!updated) return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var deleted = await _service.Delete(id);
+            if (!deleted) return NotFound();
+
+            return NoContent();
+        }
+    }
+}
